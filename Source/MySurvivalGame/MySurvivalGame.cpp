36049da81f1b3c8e0ff68c38875ff53e8daff1b3
lqh <1154546529@qq.com>
2019-10-09 09:36:40 +0800

    创建工程，添加资源，创建基础的类(SGameMode,SPlayerCharacter,SCharacter,SHUD,SCharacterMovementComponent)等

---
+++ Source/MySurvivalGame/MySurvivalGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MySurvivalGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MySurvivalGame, "MySurvivalGame" );
