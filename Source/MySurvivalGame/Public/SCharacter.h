1396aa0c72a9cc730fa24f29c2ddfa80869ebf36
lqh <1154546529@qq.com>
2019-10-12 11:54:02 +0800

    添加健康值和饥饿值，创建GetHealth,GetHunger,GetMaxHunger,ConsumeFood,IsAlive,IncrementHunger等函数

--- Source/MySurvivalGame/Public/SCharacter.h
+++ Source/MySurvivalGame/Public/SCharacter.h
@@ -24,7 +24,8 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	virtual void OnMovementModeChanged(EMovementMode PrevMovementMode, uint8 PreviousCustomMode /* = 0 */);
+	virtual void OnMovementModeChanged(EMovementMode PrevMovementMode, uint8 PreviousCustomMode /* = 0 */) override;
+	virtual float TakeDamage(float Damage, struct FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
 public:
 	UFUNCTION()
 		void MoveForward(float val);
@@ -58,7 +59,20 @@ public:
 		bool IsTargeting() const;
 	UFUNCTION(Server, Reliable, WithValidation)
 		void ServerSetTargeting(bool NewTargeting);
-
+	UFUNCTION(BlueprintCallable, Category = Targeting)
+		FRotator GetAimOffsets() const;
+	UFUNCTION(BlueprintCallable, Category = PlayerCondition)
+		float GetHealth() const;
+	UFUNCTION(BlueprintCallable, Category = PlayerCondition)
+		float GetMaxHealth() const;
+	UFUNCTION(BlueprintCallable, Category = PlayerCondition)
+		float GetHunger() const;
+	UFUNCTION(BlueprintCallable, Category = PlayerCondition)
+		float GetMaxHunger() const;
+	UFUNCTION(BlueprintCallable, Category = PlayerCondition)
+		void ConsumeFood(float AmountRestored);
+	UFUNCTION(BlueprintCallable, Category = PlayerCondition)
+		bool IsAlive() const;
 public:
 	void SetIsJumping(bool NewJumping);
 	void SetIsSprinting(bool NewSprintint);
@@ -67,6 +81,7 @@ public:
 	void OnStartTargeting();
 	void OnStopTargeting();
 	void SetTargeting(bool NewTargeting);
+	void IncrementHunger();
 
 public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
@@ -85,6 +100,37 @@ public:
 		float MaxUseDistance;
 	UPROPERTY(Transient, Replicated)
 		bool bIsTargeting;
+
+	//当前健康
+	UPROPERTY(EditDefaultsOnly, Category = PlayerCondition, Replicated)
+		float Health;
+
+	//当前饥饿
+	UPROPERTY(EditDefaultsOnly, Category = PlayerCondition, Replicated)
+		float Hunger;
+
+	//最大饥饿度
+	UPROPERTY(EditDefaultsOnly, Category = PlayerCondition)
+		float MaxHunger;
+
+	//饥饿增长时间间隔
+	UPROPERTY(EditDefaultsOnly, Category = PlayerCondition)
+		float IncrementHungerInterval;
+
+	//饥饿增长数值
+	UPROPERTY(EditDefaultsOnly, Category = PlayerCondition)
+		float IncrementHungerAmount;
+
+	//饥饿临界值
+	UPROPERTY(EditDefaultsOnly, Category = PlayerCondition)
+		float CriticalHungerThreshold;
+
+	//饥饿时掉血
+	UPROPERTY(EditDefaultsOnly, Category = PlayerCondition)
+		float HungerDamagePerInterval;
+
+	UPROPERTY(EditDefaultsOnly, Category = PlayerCondition)
+		TSubclassOf<UDamageType> HungerDamageType;
 private:
 	bool bHasNewFocus;
 	UPROPERTY()
